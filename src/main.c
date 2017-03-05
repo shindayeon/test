bb82cf61c11a4a6303d054a775fa9c5170b3f874
신다연 <sdyany@naver.com>
2017-03-05 22:25:48 +0900

    안녕할 대상에 대한 디폴트값 설정

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, const char** argv){
-const char* name = argc > 1 ? argv[1]: "World";
+const char* name = argc > 1 ? argv[1]:"World";
 printf("Hello, %s\n", name);
 return 0;
 }
